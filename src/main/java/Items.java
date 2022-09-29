dcdcbb9378352d600ede5a9a115b129f4ffe906b
amirah2700 <amir2700@hotmail.com>
2022-09-29 13:49:44 +0200

    Item klasse lavet

---
+++ src/main/java/Items.java
@@ -0,0 +1,13 @@
+public class Items {
+
+    private Room Items;
+    public Items() {
+        Items sword = new Items();
+        Items key = new Items();
+        Items chest = new Items();
+        Items lighter = new Items();
+        Items cloth = new Items();
+
+    }
+
+}
